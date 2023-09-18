c3d391ff7cf29caf59001100fd34c96f47ad543e
devpark435 <devpark435@gmail.com>
2023-09-18 22:26:39 +0900

    Initial Commit

---
+++ CardSlider/CardSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardSliderApp.swift
+//  CardSlider
+//
+//  Created by 박현렬 on 2023/09/18.
+//
+
+import SwiftUI
+
+@main
+struct CardSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardSlider/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CardSlider
+//
+//  Created by 박현렬 on 2023/09/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
