6fc7aa6095576a74a297fc910349e705cecde4bf
devpark435 <devpark435@gmail.com>
2023-09-18 23:58:35 +0900

    Design : home ui

--- CardSlider/ContentView.swift
+++ CardSlider/ContentView.swift
@@ -9,13 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            LinearGradient(gradient: .init(colors: [Color.secondary,Color.teal,]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            
+            ScrollView(.vertical, showsIndicators: false){
+                VStack{
+                    HStack{
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "line.3.horizontal").resizable().frame(width: 25,height: 25).foregroundColor(.white)
+                        }
+                        Spacer()
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "magnifyingglass").resizable().frame(width: 25,height: 25).foregroundColor(.white)
+                        }
+                        
+                    }.padding(EdgeInsets(top: 10, leading: 20, bottom: 0, trailing: 20))
+                    HStack{
+                        Text("Favorite").font(.system(size:40,weight: .bold))
+                            .foregroundColor(.white)
+                        Spacer(minLength: 0)
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "repeat").resizable().frame(width: 25,height: 25).foregroundColor(.white)
+                        }
+                        
+                    }.padding()
+                    HStack{
+                        Text("Back-End").font(.caption).foregroundColor(Color.white).fontWeight(.bold).padding(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10)).background(Color.red).opacity(0.5).clipShape(Capsule())
+                        Spacer()
+                    }.padding(.leading,20)
+                    ZStack{
+                        
+                    }
+                    Spacer()
+                }
+                
+                
+            }
+            
         }
-        .padding()
     }
 }
 
